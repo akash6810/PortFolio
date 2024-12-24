529dfd90d7ef4087be29892df8d33a85ab7ce4b9
akash6810 <126563565+akash6810@users.noreply.github.com>
2024-12-24 13:33:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,215 @@
+
+
+
+// WRONG CODE starts here
+
+// var typed = new Typed(".typing",{
+//     strings: ["Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber"],
+//     typespeed: 80,
+//     backspeed: 60,
+
+// });
+
+
+// var typed = new Typed(".typing-2",{
+//     strings: ["Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+//     "Coder","Programmer","Blogger","Youtuber"],
+//     typespeed: 100,
+//     backspeed: 60,
+
+// });
+
+// WRONG CODE ends here
+
+
+
+
+
+// JavaScript : If you can find another way to do that then plzz send me the code :) : akash.04mon@gmail.com
+var typed = new Typed(".typing", {
+    strings: [
+        "Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber",
+    "Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber"
+    ],
+    typeSpeed: 80, // Speed of typing in milliseconds
+    backSpeed: 60, // Speed of deleting in milliseconds
+    loop: true // Whether to loop the animation or not
+});
+
+var typed = new Typed(".typing-2", {
+    strings: [
+        "Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber",
+    "Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber","Web Designer","Web Developer",
+    "Coder","Programmer","Blogger","Youtuber"
+    ],
+    typeSpeed: 80, // Speed of typing in milliseconds
+    backSpeed: 60, // Speed of deleting in milliseconds
+    loop: true // Whether to loop the animation or not
+});
+/* 
+// // Show/Hide FAQs answer
+// const faqs = document.querySelectorAll('.faq');
+// faqs.forEach(faq => {
+//     faq.addEventListener('click', () => {
+//         faq.classList.toggle('open');
+//         //change icon
+//         const icon = faq.querySelector('.
+//         faq_icon i');
+//         if (icon.className === 'fa-solid
+//         fa-plus'){
+//         icon.className = 'fa-solid fa-minus';
+//         }
+//         else{
+//         icon.className = 'fa-solid fa-plus';
+//         }
+//     })
+// })
+
+
+*/
+
+const menu = document.querySelector('.nav_menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+
+})
+
+//close nav menu
+
+const closeNav =()=>{
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+
+}
+
+closeBtn.addEventListener('click',closeNav)
+
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.
+    toggle('window-scroll',window.scrollY>100);
+})
